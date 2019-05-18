b51d06a75e326c2fba6c7a73af604a50b573136f
root <root@vps686095.ovh.net>
2019-05-18 12:40:08 +0200

    first commit

---
+++ server/workerServer.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+import socket
+import threading
+
+
+class ClientThread(threading.Thread):
+
+    def __init__(self, ip, port, clientsocket):
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.clientsocket = clientsocket
+        print("[+] Nouveau thread pour %s %s" % (self.ip, self.port,))
+
+    def run(self):
+        print("Connexion de %s %s" % (self.ip, self.port,))
+
+        r = self.clientsocket.recv(99999)
+        print("Ouverture du fichier", "...")
+        print(r)
+
+        self.clientsocket.send('Fichier correctement recu'.encode('utf-8'))
+
+        print("Client déconnecté...")
+
+
+tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcpsock.bind(("", 1111))
+
+while True:
+    tcpsock.listen(10)
+    print("En écoute...")
+    (clientsocket, (ip, port)) = tcpsock.accept()
+    newthread = ClientThread(ip, port, clientsocket)
+    newthread.start()
