b0d272617c9d0ff23a48cc38aa6752cb78a06f75
root <root@vps686095.ovh.net>
2019-07-01 14:07:34 +0200

    sql script fixed

--- usine_automate/prod/usine-db-backup.py
+++ usine_automate/prod/usine-db-backup.py
@@ -8,7 +8,8 @@ database = 'devops'
 
 date = calendar.timegm(time.gmtime()) 
 dumpname = 'database_usine'+str(date)+'.sql'
+savepath = '/home/kalus/sql_dump/'
 
-with open(dumpname,'w+') as output:
+with open(savepath+dumpname,'w+') as output:
   c = subprocess.Popen('mysqldump -u ' + username + ' -p' + password + ' ' +database,
                        stdout=output, shell=True)
